ef141cde31f9ac436228c62cfd41537c4894c799
Jonathan Bridge <jvbridge@gmail.com>
2022-04-22 16:36:29 -0700

    added manager prompt

--- __tests__/Prompter.test.js
+++ __tests__/Prompter.test.js
@@ -1,20 +1,33 @@
 const Prompter = require("../lib/Prompter");
+const Inquirer = require("inquirer");
+const Manager = require("../lib/Manager");
+
+jest.mock("inquirer");
+
 
 describe ("Prompter", () => {
     const prompt = new Prompter();
     
-    // describe("prompt employee", () => {
-    //     it("should prompt the user to create a new employee", () => {
-    //         const currEmployees = prompt.employees.length;
-    //         prompt.askEmployee();
-    //         expect(prompt.employees.length).isEqual(currEmployees + 1);
-    //     });
-    // });
+    
+
     describe("prompt manager", () => {
-        test("should prompt the user to create a new manager", async () => {
-            const currEmployees = prompt.employees.length;
-            await expect(prompt.askManager());
-            expect(prompt.employees.length).toBe(currEmployees + 1);
+        it ("should prompt the user to create a new manager", async () => {
+            const currLen = prompt.employees.length;
+            const answers = {
+                name: "John Doe", 
+                id:"429", 
+                email:"JohnDoe@example.com", 
+                officeNumber: 429
+            };
+            // inquirer returns a promise 
+            Inquirer.prompt.mockReturnValue(new Promise((resolve)=>{return answers;}));
+            
+            // after the promise resolves run the test
+            prompt.askManager().then(() =>{
+                expect(prompt.employees.length).toEqual(currLen + 1);
+                expect(prompt.employees[currLen] instanceof Manager).toEqual(true);
+            });
+            
         });
     });
 });

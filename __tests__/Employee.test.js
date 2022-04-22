8296a2bb241106a37b901d2ba2aa824fd0f18e62
Jonathan Bridge <jvbridge@gmail.com>
2022-04-21 18:04:58 -0700

    Finished base employee functionality

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require("../lib/Employee");
 
-describe('Employee', () =>{
+describe('Employee', () => {
     
     it("should have a name, id, and email strings", ()=>{
         const emp = new Employee("john Doe", "0x2fba", "JohnDoe@example.com");
@@ -9,4 +9,34 @@ describe('Employee', () =>{
         expect(typeof emp.email).toEqual("string");
     });
 
+    describe('getName', () =>{
+        it ("should return the name of the employee", () =>{
+            const name = "john doe";
+            const emp = new Employee(name, "0x2fba", "JohnDoe@example.com");
+            expect(emp.getName()).toEqual(name);
+        });
+    });
+
+    describe('getId', () =>{
+        it ("should return the name of the employee", () =>{
+            const id = "0x2fba";
+            const emp = new Employee("john doe", id, "JohnDoe@example.com");
+            expect(emp.getId()).toEqual(id);
+        });
+    });
+    
+    describe('getEmail', () =>{
+        it ("should return the name of the employee", () =>{
+            const email = "JohnDoe@example.com";
+            const emp = new Employee("john doe", "0x2fba", email);
+            expect(emp.getEmail()).toEqual(email);
+        });
+    });
+
+    describe('getRole', () =>{
+        it ("should return the name of the employee", () =>{
+            const emp = new Employee("john doe", "0x2fba", "JohnDoe@example.com");
+            expect(emp.getRole()).toEqual("Employee");
+        });
+    });
 });

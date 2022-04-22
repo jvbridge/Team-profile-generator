aeb5d69fd828cf1f7748dfbfe3b3de9f09371c62
Jonathan Bridge <jvbridge@gmail.com>
2022-04-22 12:50:53 -0700

    swapping branches to reconfigure

--- __tests__/Prompter.test.js
+++ __tests__/Prompter.test.js
@@ -3,11 +3,18 @@ const Prompter = require("../lib/Prompter");
 describe ("Prompter", () => {
     const prompt = new Prompter();
     
-    describe("prompt employee", () => {
-        it("should prompt the user to create a new employee", () => {
+    // describe("prompt employee", () => {
+    //     it("should prompt the user to create a new employee", () => {
+    //         const currEmployees = prompt.employees.length;
+    //         prompt.askEmployee();
+    //         expect(prompt.employees.length).isEqual(currEmployees + 1);
+    //     });
+    // });
+    describe("prompt manager", () => {
+        test("should prompt the user to create a new manager", async () => {
             const currEmployees = prompt.employees.length;
-            prompt.askEmployee();
-            expect(prompt.employees.length).isEqual(currEmployees + 1);
+            await expect(prompt.askManager());
+            expect(prompt.employees.length).toBe(currEmployees + 1);
         });
     });
 });
